5e63a6400691084676ec4d2d524ca97aa427a04c
HiuHiu <dh51703400@student.stu.edu.vn>
2021-11-15 14:47:31 +0700

    KTCHANLE

--- source.cpp
+++ source.cpp
@@ -1,28 +1,8 @@
 #include <iostream>
 using namespace std;
-int hieu(int a, int b)
-{
-return (a-b);
-};
-float Tich(int a,int b)
-{
-return (a*b);
-}
 int main()
 {
-	int a, b;
-	char phepToan2;
-	cout << "Nhap so nguyen 1: ";
-	cin >> a;
-	cout << "Nhap so nguyen 2: ";
-	cin >> b;
-	cout << "Chon phep toan (+,-,*) ";
-	cin >> phepToan2;
-	if (phepToan2 == "-")
-	  cout << "\na - b = " << hieu(a,b) << "\n";
-	//Tinh Tich
-	if(phepToan2 =="*")
-		cout <<"\a*b="<<tich(a,b)<<"\n";
+	
 	//KiemtrasoChanle
  	int num;
   
